33efc7d1462587235670348f39dede9d1c2c766c
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2024-01-08 11:51:52 +0800

    modification in hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <div class="min-h-screen hero bg-blue-50">
 	<div class="text-center hero-content">
 		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">Hello there</h1>
-			<p class="py-6">
-				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			<h1
+				class="text-2xl font-bold text-transparent bg-gradient-to-r from-blue-500 via-sky-500 to-pink-500 bg-clip-text"
+			>
+				Connect. Learn. Collaborate
+			</h1>
+			<p class="py-6 font-semibold">
+				Welcome to <span
+					class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-sky-500"
+					>Divspace</span
+				> , a social interaction platform where students, faculty members, and alumni come together to
+				forge meaningful connections and foster a dynamic learning environment.
 			</p>
 			<button class="btn btn-primary">Get Started</button>
 		</div>
